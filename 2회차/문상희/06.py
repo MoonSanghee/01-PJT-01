3cf0c6312f42b79b2d0f99ed2b8d313d2a352962
MoonSanghee <psycho13th@gmail.com>
2022-07-15 22:36:43 +0900

    프로젝트 06까지 완료

--- 2회차/문상희/06.py
+++ 2회차/문상희/06.py
@@ -5,7 +5,23 @@ from pprint import pprint
 def movie_info(movies, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    movie_col = []
+    for movie in movies:
+        genre_ids = movie['genre_ids']
+        names = []
+        for genre in genres:
+            if genre['id'] in genre_ids:
+                names.append(genre['name'])
+
+        movie_info = {
+            'genre_names' : names,        
+            'id': movie.get('id'),
+            'title' : movie.get('title'),
+            'vote_average' : movie.get('vote_average'),
+            'overview' : movie.get('overview')
+        }
+        movie_col.append(movie_info)
+    return movie_col
         
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
